2bacd1978ceefd321d1f1c8f0fb30f70f47d4e0f
Bengilata <130096478+Bengilata@users.noreply.github.com>
2023-06-15 23:44:24 +0300

    Jump tamam fakat animasyon çalışmıyor
    jump ın animasyonu dışında her şey tamam. sadece sağa ve sola gitme animasyonları değişecek

--- Assets/Scripler/JumpScr.cs
+++ Assets/Scripler/JumpScr.cs
@@ -6,8 +6,9 @@ public class JumpScr : MonoBehaviour
 {
     PlayerKontrol playerControl;
     private Animator animator;
+    private bool isJumping = false; // deðiþken
 
-    float moveValue;
+    public float jumpForce = 5f;
 
     void Start()
     {
@@ -17,17 +18,43 @@ public class JumpScr : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            
+            Jump();
+        }
+
         SetAnim();
     }
 
     void SetAnim()
     {
-        moveValue = playerControl.movement.magnitude;
-        
-
+        float moveValue = playerControl.movement.magnitude;
         animator.SetFloat("Speed", moveValue);
         animator.SetFloat("Yatay", playerControl.movement.y);
         animator.SetFloat("Dikey", playerControl.movement.x);
     }
 
+    void Jump()
+    {
+        
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+        isJumping = true;
+        animator.SetBool("IsJumping", true); // Zýplama animasyonunu etkinleþti
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Yere temas ettiðinde durum sýfýrlanrý
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+            animator.SetBool("IsJumping", false); // Zýplama animasyonunu devre dýþý
+        }
+    }
 }
+
