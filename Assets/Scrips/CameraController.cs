ec86d78e64469686038c7cb636335e048841d1ca
Saner AYKANAT <123635760+Saner-AYKANAT@users.noreply.github.com>
2023-06-16 16:20:26 +0300

    kamera kontroller eklendi

---
+++ Assets/Scrips/CameraController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform hedef;
+    public Vector3 hedefMesafe;
+    public float hassasiyet ;
+
+    float fareX, fareY;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, hedef.position + hedefMesafe, Time.deltaTime * 10);
+        fareX += Input.GetAxis("Mouse X") * hassasiyet;
+        fareY += Input.GetAxis("Mouse Y") * hassasiyet;
+
+        if (fareY>=25)
+        {
+            fareY = 25;
+        }
+
+        if (fareY<=-25)
+        {
+            fareY =-25;
+        }
+
+        this.transform.eulerAngles = new Vector3(fareY, fareX, 0);
+        hedef.transform.eulerAngles = new Vector3(0, fareX, 0);
+    }
+}
---
+++ Assets/Scrips/PlayerKontrol.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
+
+public class PlayerKontrol : MonoBehaviour
+{
+    Animator anim;
+
+    [SerializeField]
+    private float speed;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float yatayHareket = Input.GetAxis("Horizontal");  // Yatay hareket girişini al
+        float dikeyHareket = Input.GetAxis("Vertical");  // Dikey hareket girişini al
+
+        Vector3 hareket = new Vector3(yatayHareket*speed, 0f, dikeyHareket*speed);  // Hareket vektörünü oluştur
+
+        //if (hareket.magnitude > 0f)  // Eğer karakter hareket ediyorsa
+        //{
+        //    Quaternion yeniYon = Quaternion.LookRotation(hareket);  // Hareket yönünü belirle
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, yeniYon, 0.15f);  // Yavaşça dön
+
+        //    gameObject.transform.Translate(yatayHareket*Time.deltaTime,0,dikeyHareket*Time.deltaTime);
+        //}
+
+        anim.SetFloat("speed", hareket.magnitude);
+
+    }
+}
+
+
