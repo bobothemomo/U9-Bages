2a1aa52d440aec6ad8dffc6ee55d4f2274d35657
Alper <hayrettinalpersapan@gmail.com>
2023-06-23 21:40:23 +0300

    xxx

--- Assets/Alper/Scripts/AirBallAnimController.cs
+++ Assets/Alper/Scripts/AirBallAnimController.cs
@@ -9,6 +9,8 @@ public class AirBallAnimController : MonoBehaviour
     public GameObject topPrefab; // Fýrlatýlacak topun prefab'i
     public float firlatmaMesafesi = 1f; // Topun karakterin transformundan çýkýþ mesafesi
     public float firlatmaHizi = 10f; // Topun fýrlatma hýzý
+
+    bool airBallActive = false;
     private void Start()
     {
         anim = GetComponent<Animator>();
@@ -22,23 +24,33 @@ public class AirBallAnimController : MonoBehaviour
 
     void AirBall()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {   
+        if (Input.GetKeyDown(KeyCode.R) && airBallActive == false)
+        {
             Debug.Log("r bastýn");
             anim.SetTrigger("throwAir");
-            
-            // Topun doðacaðý pozisyonu hesapla
-            Vector3 firlatmaPozisyonu = transform.position + (transform.up * firlatmaMesafesi);
+            StartCoroutine(WaitAirBall());
 
-            // Topu doðru pozisyonda oluþtur
-            GameObject firlatilanTop = Instantiate(topPrefab, firlatmaPozisyonu, transform.rotation);
+            IEnumerator WaitAirBall()
+            {
+                airBallActive = true;
+                yield return new WaitForSeconds(1.5f);
+                Debug.Log("2f bekledi");
 
-            // Fýrlatýlan topa hýz ver
-            Rigidbody topRigidbody = firlatilanTop.GetComponent<Rigidbody>();
-            topRigidbody.velocity = transform.forward * firlatmaHizi;
+                // Topun doðacaðý pozisyonu hesapla
+                Vector3 firlatmaPozisyonu = transform.position + (transform.up * firlatmaMesafesi);
+
+                // Topu doðru pozisyonda oluþtur
+                GameObject firlatilanTop = Instantiate(topPrefab, firlatmaPozisyonu, transform.rotation);
+                // Fýrlatýlan topa hýz ver
+                Rigidbody topRigidbody = firlatilanTop.GetComponent<Rigidbody>();
+                topRigidbody.velocity = transform.forward * firlatmaHizi;
+                yield return new WaitForSeconds(3f);
+                airBallActive = false;
+
+            }
 
         }
     }
-   
+
 
 }
