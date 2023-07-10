55fefc166004f48c9757dfcfa8d9806b593d2bdf
Bengilata <130096478+Bengilata@users.noreply.github.com>
2023-07-10 22:02:15 +0300

    scriptler düzeltildi

--- Assets/Scrips/BengiScripts/Door.cs
+++ Assets/Scrips/BengiScripts/Door.cs
@@ -1,32 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Door : MonoBehaviour
 {
-
+    [SerializeField] GameObject warningPart;
+    [SerializeField] TextMeshProUGUI warningText;
     public bool isOpen = false;
-    public GameObject player;
 
-    
+    [SerializeField] float speed;
 
 
     void Start()
     {
-        player = GameObject.Find("Player");
-
     }
 
-
-    public void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (isOpen == true)
+        if (!isOpen && other.gameObject.CompareTag("Player"))
         {
-            transform.Rotate(Vector3.up * -90);
+            warningPart.SetActive(true);
+            warningText.text = "Kapý kilitli!";
+        }
+        else if (isOpen && other.gameObject.CompareTag("Player"))
+        {
+            
+            StartCoroutine(DoorOpening());
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if(!isOpen && other.gameObject.CompareTag("Player"))
+        {
+            
+        }
+    }
+
+    IEnumerator DoorOpening()
+    {
+        while(transform.localEulerAngles.y >= 180f)
+        {
+            transform.Rotate(Vector3.up * speed);
+
+            yield return null;
         }
 
-     
     }
 
     
--- Assets/Scrips/BengiScripts/Mesale.cs
+++ Assets/Scrips/BengiScripts/Mesale.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Mesale : MonoBehaviour
 {
     public Door door;
-    
-    
+
+    [SerializeField] GameObject warningPart;
+    [SerializeField] TextMeshProUGUI warningText;
+
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             door.isOpen = true;
         }       
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            warningText.text = "Kapý açýldý";
+            
+            
+        }
     }
 }
